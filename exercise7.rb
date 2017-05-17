ec6ece5ed76ab1367f2e96841ba082870808e2f3
Ronen Annason <ronenannason83@gmail.com>
2017-05-17 15:42:18 -0400

    exercise 7 complete

---
+++ exercise7.rb
@@ -0,0 +1,11 @@
+def wrap_text(wrapper,string)
+ puts "#{wrapper}#{string}#{wrapper.reverse}"
+end
+
+wrap_text("---###===", "New Message")
+
+#wrap_text(string)
+#string, "---###==="
+#puts "#{string}"
+
+#wrap_text("new message")
