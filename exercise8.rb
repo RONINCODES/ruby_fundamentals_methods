453a723fc357a317b2d3f83c92975822a19cad8c
Ronen Annason <ronenannason83@gmail.com>
2017-05-17 16:34:24 -0400

    comleted exercise 8

--- exercise8.rb
+++ exercise8.rb
@@ -1,10 +1,11 @@
-def wrap_text(string, wrapper)
- return "#{wrapper}#{string}#{wrapper}"
+expenses = [25.5, 32.8, 30, 99.99]
+expenses1 = [100.00, 2.00, 3000.00, 6]
+expenses2 = [99.00, 98.00, 97.00, 96.00]
+
+def sum_of_array(x)
+  x.sum
 end
 
-puts wrap_text('hello', '===')
-
-#wrap_text 'hello', '==='
-
-
-# ===hello===
+puts sum_of_array(expenses)
+puts sum_of_array(expenses1)
+puts sum_of_array(expenses2)
